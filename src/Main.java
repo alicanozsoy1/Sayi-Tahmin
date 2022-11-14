258d2d84da33e60f93c38cd9f4f7d34fdae6d6d8
alicanozsoy1 <alicanozsoy11@gmail.com>
2022-11-14 12:06:20 +0300

    oyuna 3 hak tanımlandı

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,33 @@ public class Main {
         Scanner klavye = new Scanner(System.in);
 
         //SAYİ TAHMİN ETME OYUNU
-        System.out.println("bir sayı tahmin ediniz:");
+        /*System.out.println("bir sayı tahmin ediniz:");
         int tahmin = klavye.nextInt();
         int bil_secim = (int)(Math.random()*10);
+        int hak = 3;
 
-        while (tahmin != bil_secim){
-            if (tahmin<0 || tahmin>10){
-                System.out.println("geçersiz değer");
-                 tahmin = klavye.nextInt();
-            } else if (tahmin>bil_secim) {
-                System.out.println("tahmininiz: "+tahmin+" -> Daha küçük sayi giriniz");
-                 tahmin = klavye.nextInt();
+        while (hak>1){
+
+            if (tahmin<0 || tahmin>10) {
+                System.out.println("geçersiz değer, tekrar tahmin ediniz: ");
+                hak--;
+                tahmin = klavye.nextInt();
+
+            } else if (tahmin == bil_secim) {
+                System.out.println("kazandınız");
+            }
+            else if (tahmin>bil_secim) {
+                System.out.println("tahmininiz: " +tahmin+ " -> Daha küçük sayi giriniz: ");
+                hak --;
+                tahmin = klavye.nextInt();
+
             } else {
-                System.out.println("tahmininiz: "+tahmin+" -> Daha büyük sayi giriniz");
+                System.out.println("tahmininiz: " +tahmin+ " -> Daha büyük sayi giriniz: ");
+                hak --;
                 tahmin = klavye.nextInt();
             }
         }
-        System.out.println("bilgisayarın sayısı: "+bil_secim+" tahmininiz: "+tahmin);
-        System.out.println("Tebrikler,kazandınız");
-
-
-
+        System.out.println("kaybettiniz, bil_secim: " + bil_secim);*/
 
     }
 }
